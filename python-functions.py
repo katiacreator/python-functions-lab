203e37076947cdd1dd4dd4b603f0675af2c66bb7
katiacreator <katia.creator@gmail.com>
2021-08-24 20:01:27 -0400

    solution to ex 2

--- python-functions.py
+++ python-functions.py
@@ -6,3 +6,9 @@ def sum_to(n):
 print(sum_to(6))
 print(sum_to(10))
 
+def largest(list):
+  list.sort()
+  return list[-1]
+
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 231, 91, 54]))
